78aa976795c9673cbe87fb5887db344d05bb60ba
sanyogyadav <sanyogyadav52@gmail.com>
2023-04-03 15:27:07 +0530

    [SANYOG]-Model created for wishlist and cart

---
+++ model/cart.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const CartSchema = new mongoose.Schema({
+
+    _userId: { type: String, required: true },
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product", },],
+
+})
+
+const Cart = mongoose.model('Cart', CartSchema)
+
+module.exports = Cart;
---
+++ model/fav.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const FavSchema = new mongoose.Schema({
+    
+  _userId: { type: String, required: true },
+  products: [  {type: mongoose.Schema.Types.ObjectId, ref: "Product", }]
+ 
+});
+const Fav = mongoose.model("Favourite", FavSchema);
+
+module.exports = Fav;
